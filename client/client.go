e0bded8f5738ff3f2ef718a0948ea0a35d1c4c65
Laszlo Fogas <laszlo@laszlo.cloud>
2021-03-09 15:26:48 +0100

    Returning the event ID for tracking

--- client/client.go
+++ client/client.go
@@ -213,7 +213,7 @@ func (c *client) ReleasesGet(
 // ReleasesPost releases the given artifact to the given environment
 func (c *client) ReleasesPost(env string, artifactID string) (string, error) {
 	uri := fmt.Sprintf(pathReleases + "?env=%s&artifact=%s", c.addr, env, artifactID)
-	var result map[string]interface{}
+	result := make(map[string]interface{})
 	err := c.post(uri, nil, result)
 	if err != nil {
 		return "", nil
@@ -224,7 +224,7 @@ func (c *client) ReleasesPost(env string, artifactID string) (string, error) {
 // ReleasesPost releases the given artifact to the given environment
 func (c *client) RollbackPost(env string, app string, targetSHA string) (string, error) {
 	uri := fmt.Sprintf(pathRollback + "?env=%s&app=%s&sha=%s", c.addr, env, app, targetSHA)
-	var result map[string]interface{}
+	result := make(map[string]interface{})
 	err := c.post(uri, nil, result)
 	if err != nil {
 		return "", nil
