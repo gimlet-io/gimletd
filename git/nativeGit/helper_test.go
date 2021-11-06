1fe6f81c7ae3211fd01dc03449613bdd1824f998
Laszlo Fogas <laszlo@laszlo.cloud>
2021-11-06 19:58:23 +0000

    Fixing test

--- git/nativeGit/helper_test.go
+++ git/nativeGit/helper_test.go
@@ -1,13 +1,14 @@
 package nativeGit
 
 import (
+	"testing"
+
 	"github.com/go-git/go-billy/v5/memfs"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/storage/memory"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 func Test_Releases(t *testing.T) {
@@ -15,7 +16,7 @@ func Test_Releases(t *testing.T) {
 
 	releases, err := Releases(repo, "my-app", "staging", nil, nil, 10, "")
 	assert.Nil(t, err)
-	assert.Equal(t, 3, len(releases),"should get all releases")
+	assert.Equal(t, 3, len(releases), "should get all releases")
 }
 
 func Test_ReleasesLimit(t *testing.T) {
@@ -23,16 +24,16 @@ func Test_ReleasesLimit(t *testing.T) {
 
 	releases, err := Releases(repo, "my-app", "staging", nil, nil, 1, "")
 	assert.Nil(t, err)
-	assert.Equal(t, 1, len(releases),"should get only one release")
+	assert.Equal(t, 1, len(releases), "should get only one release")
 }
 
 func Test_ReleasesGitRepo(t *testing.T) {
 	repo := initHistory()
 
-	releases, err := Releases(repo, "my-app2", "staging", nil, nil, 0, "laszlocph/gimletd-test2")
+	releases, err := Releases(repo, "my-app2", "staging", nil, nil, -1, "laszlocph/gimletd-test2")
 	assert.Nil(t, err)
-	assert.Equal(t, 1, len(releases),"should get the commit from the gitrepo")
-	assert.Equal(t, "xxx", releases[0].App,"should get the commit from the gitrepo")
+	assert.Equal(t, 1, len(releases), "should get the commit from the gitrepo")
+	assert.Equal(t, "xxx", releases[0].App, "should get the commit from the gitrepo")
 }
 
 func Test_Status(t *testing.T) {
@@ -45,11 +46,11 @@ func Test_Status(t *testing.T) {
 
 	status, err := Status(repo, "my-app", "staging", perf)
 	assert.Nil(t, err)
-	assert.Equal(t, 1, len(status),"should get release status for app")
+	assert.Equal(t, 1, len(status), "should get release status for app")
 
 	status, err = Status(repo, "", "staging", perf)
 	assert.Nil(t, err)
-	assert.Equal(t, 2, len(status),"should get release status for all apps")
+	assert.Equal(t, 2, len(status), "should get release status for all apps")
 }
 
 func initHistory() *git.Repository {
