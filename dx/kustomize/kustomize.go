f1d8cb7c3a8eb6d7098b3837cf7efaad80eb4b49
Laszlo Fogas <laszlo@laszlo.cloud>
2021-11-22 13:09:10 +0100

    Applying kustomize patches

---
+++ dx/kustomize/kustomize.go
@@ -0,0 +1,55 @@
+package kustomize
+
+import (
+	"fmt"
+	"sigs.k8s.io/kustomize/api/filesys"
+	"sigs.k8s.io/kustomize/api/krusty"
+)
+
+func ApplyPatches(patch string, templatesManifests string) (string, error) {
+	bytePatch := []byte(fmt.Sprintf("%v", patch))
+
+	fSys := filesys.MakeFsInMemory()
+	err := fSys.WriteFile("manifests.yaml", []byte(templatesManifests))
+	if err != nil {
+		return "", err
+	}
+
+	err = fSys.WriteFile("patches.yaml", bytePatch)
+	if err != nil {
+		return "", err
+	}
+
+	err = fSys.WriteFile("kustomization.yaml", []byte(`
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+resources:
+- manifests.yaml
+patchesStrategicMerge:
+- patches.yaml
+`))
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Println(templatesManifests)
+	b := krusty.MakeKustomizer(fSys, krusty.MakeDefaultOptions())
+	resources, err := b.Run(".")
+	if err != nil {
+		return "", err
+	}
+
+	var files []byte
+	for _, res := range resources.Resources() {
+		yaml, err := res.AsYAML()
+		if err != nil {
+			return "", err
+		}
+		delimiter := []byte("---\n")
+		files = append(files, delimiter...)
+		files = append(files, yaml...)
+
+	}
+
+	return string(files), err
+}
