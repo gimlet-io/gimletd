137b30272b910828c5e0ba2dc48d7e525768e657
dzsak <sisakadam@gmail.com>
2022-02-11 15:29:48 +0100

    Add more test cases for sending messages on Discord

--- notifications/discord_test.go
+++ notifications/discord_test.go
@@ -8,60 +8,103 @@ import (
 	"github.com/gimlet-io/gimletd/worker/events"
 )
 
-// func TestDiscordBot(t *testing.T) {
-// 	p := &DiscordProvider{
-// 		Token:     "OTQxNjAzNDk5ODk5MjQ0NTc0.YgYWmA._Og1LRgO356obmm0zox3Hb4AVeo",
-// 		ChannelID: "940971232847884329",
-// 	}
-
-// 	msg := fluxMessage{}
-// 	err := p.send(&msg)
-// 	if err != nil {
-// 		t.Errorf("Sending message in Discord must not return error!")
-// 	}
-// }
-
 func TestSendingFluxMessage(t *testing.T) {
 
-	msg := fluxMessage{
+	msgHealthCheckPassed := fluxMessage{
 		gitopsCommit: &model.GitopsCommit{
 			ID:         200,
 			Sha:        "76ab7d611242f7c6742f0ab662133e02b2ba2b1c",
-			Status:     "Processing",
+			Status:     "Progressing",
 			StatusDesc: "Health check passed",
 		},
 		gitopsRepo: "gimlet",
 		env:        "staging",
 	}
 
-	discordMessage, err := msg.AsDiscordMessage()
+	discordMessageHealthCheckPassed, err := msgHealthCheckPassed.AsDiscordMessage()
 	if err != nil {
 		t.Errorf("Failed to create Discord message!")
 	}
 
-	assertEqual(t, discordMessage.Text, "Health check")
+	assertEqual(t, discordMessageHealthCheckPassed.Embed.Description, ":heavy_check_mark: Applied resources from [76ab7d6](https://github.com/gimlet/commit/76ab7d611242f7c6742f0ab662133e02b2ba2b1c) are up and healthy")
+
+	msgHealthCheckProgressing := fluxMessage{
+		gitopsCommit: &model.GitopsCommit{
+			ID:         200,
+			Sha:        "76ab7d611242f7c6742f0ab662133e02b2ba2b1c",
+			Status:     "Progressing",
+			StatusDesc: "progressing",
+		},
+		gitopsRepo: "gimlet",
+		env:        "staging",
+	}
+
+	discordMessageHealthCheckProgressing, err := msgHealthCheckProgressing.AsDiscordMessage()
+	if err != nil {
+		t.Errorf("Failed to create Discord message!")
+	}
+
+	assertEqual(t, discordMessageHealthCheckProgressing.Embed.Description, ":hourglass_flowing_sand: Applying gitops changes from [76ab7d6](https://github.com/gimlet/commit/76ab7d611242f7c6742f0ab662133e02b2ba2b1c)")
+
+	msgHealthCheckFailed := fluxMessage{
+		gitopsCommit: &model.GitopsCommit{
+			ID:         200,
+			Sha:        "76ab7d611242f7c6742f0ab662133e02b2ba2b1c",
+			Status:     "ReconciliationFailed",
+			StatusDesc: "progressing",
+		},
+		gitopsRepo: "gimlet",
+		env:        "staging",
+	}
+
+	discordMessageHealthCheckFailed, err := msgHealthCheckFailed.AsDiscordMessage()
+	if err != nil {
+		t.Errorf("Failed to create Discord message!")
+	}
+
+	assertEqual(t, discordMessageHealthCheckFailed.Embed.Description, ":exclamation: Gitops changes from [76ab7d6](https://github.com/gimlet/commit/76ab7d611242f7c6742f0ab662133e02b2ba2b1c) failed to apply")
 
 }
 
 func TestSendingGitopsDeleteMessage(t *testing.T) {
 
-	msg := gitopsDeleteMessage{
+	msgDeleteFailed := gitopsDeleteMessage{
 		event: &events.DeleteEvent{
 			Env:         "staging",
 			App:         "myapp",
 			TriggeredBy: "Gimlet",
-			StatusDesc:  "",
+			StatusDesc:  "cannot delete",
 			GitopsRef:   "76ab7d611242f7c6742f0ab662133e02b2ba2b1c",
 			GitopsRepo:  "testrepo",
+			Status:      1,
 		},
 	}
 
-	discordMessage, err := msg.AsDiscordMessage()
+	discordMessageDeleteFailed, err := msgDeleteFailed.AsDiscordMessage()
 	if err != nil {
 		t.Errorf("Failed to create Discord message!")
 	}
 
-	assertEqual(t, discordMessage.Text, "Gimlet is deleting myapp on staging")
+	assertEqual(t, discordMessageDeleteFailed.Embed.Description, ":exclamation: *Error* :exclamation: cannot delete")
+
+	msgPolicyDeletion := gitopsDeleteMessage{
+		event: &events.DeleteEvent{
+			Env:         "staging",
+			App:         "myapp",
+			TriggeredBy: "policy",
+			StatusDesc:  "cannot delete",
+			GitopsRef:   "76ab7d611242f7c6742f0ab662133e02b2ba2b1c",
+			GitopsRepo:  "testrepo",
+			Status:      2,
+		},
+	}
+
+	discordMessagePolicyDeletion, err := msgPolicyDeletion.AsDiscordMessage()
+	if err != nil {
+		t.Errorf("Failed to create Discord message!")
+	}
+
+	assertEqual(t, discordMessagePolicyDeletion.Text, "Policy based deletion of myapp on staging")
 
 }
 
@@ -72,7 +115,31 @@ func TestSendingGitopsDeployMessage(t *testing.T) {
 		URL:            "https://gimlet.io",
 	}
 
-	msg := gitopsDeployMessage{
+	msgSendFailure := gitopsDeployMessage{
+		event: &events.DeployEvent{
+			Manifest: &dx.Manifest{
+				App: "myapp",
+				Env: "staging",
+			},
+			Artifact: &dx.Artifact{
+				Version: *version,
+			},
+			TriggeredBy: "Gimlet",
+			Status:      1,
+			StatusDesc:  "",
+			GitopsRef:   "76ab7d611242f7c6742f0ab662133e02b2ba2b1c",
+			GitopsRepo:  "testrepo",
+		},
+	}
+
+	discordMessageSendFailure, err := msgSendFailure.AsDiscordMessage()
+	if err != nil {
+		t.Errorf("Failed to create Discord message!")
+	}
+
+	assertEqual(t, discordMessageSendFailure.Text, "Failed to roll out myapp of testrepo")
+
+	msgSendByGimlet := gitopsDeployMessage{
 		event: &events.DeployEvent{
 			Manifest: &dx.Manifest{
 				App: "myapp",
@@ -89,18 +156,40 @@ func TestSendingGitopsDeployMessage(t *testing.T) {
 		},
 	}
 
-	discordMessage, err := msg.AsDiscordMessage()
+	discordMessageSendByGimlet, err := msgSendByGimlet.AsDiscordMessage()
 	if err != nil {
 		t.Errorf("Failed to create Discord message!")
 	}
 
-	assertEqual(t, discordMessage.Text, "Gimlet is rolling out myapp on testrepo")
+	assertEqual(t, discordMessageSendByGimlet.Text, "Gimlet is rolling out myapp on testrepo")
 
 }
 
 func TestSendingGitopsRollbackMessage(t *testing.T) {
 
-	msg := gitopsRollbackMessage{
+	msgRollbackFailed := gitopsRollbackMessage{
+		event: &events.RollbackEvent{
+			RollbackRequest: &dx.RollbackRequest{
+				Env:         "staging",
+				App:         "myapp",
+				TargetSHA:   "76ab7d611242f7c6742f0ab662133e02b2ba2b1c",
+				TriggeredBy: "Gimlet",
+			},
+			Status:     1,
+			StatusDesc: "success",
+			GitopsRefs: []string{"76ab7d611242f7c6742f0ab662133e02b2ba2b1c", "76ab7d611242f7c6742f0ab662133e02b2ba2bbb", "76ab7d611242f7c6742f0ab662133e02b2ba2lll"},
+			GitopsRepo: "gimlet-io",
+		},
+	}
+
+	discordMessageRollbackFailed, err := msgRollbackFailed.AsDiscordMessage()
+	if err != nil {
+		t.Errorf("Failed to create Discord message!")
+	}
+
+	assertEqual(t, discordMessageRollbackFailed.Text, "Failed to roll back myapp of staging")
+
+	msgRollbackSuccess := gitopsRollbackMessage{
 		event: &events.RollbackEvent{
 			RollbackRequest: &dx.RollbackRequest{
 				Env:         "staging",
@@ -115,12 +204,12 @@ func TestSendingGitopsRollbackMessage(t *testing.T) {
 		},
 	}
 
-	discordMessage, err := msg.AsDiscordMessage()
+	discordMessageRollbackSuccess, err := msgRollbackSuccess.AsDiscordMessage()
 	if err != nil {
 		t.Errorf("Failed to create Discord message!")
 	}
 
-	assertEqual(t, discordMessage.Text, "🔙 Gimlet is rolling back myapp on staging")
+	assertEqual(t, discordMessageRollbackSuccess.Text, "🔙 Gimlet is rolling back myapp on staging")
 
 }
 
