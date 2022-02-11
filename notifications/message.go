b74168e89e4555813582018deb3900eaaaee3841
Ádám <sisakadam@gmail.com>
2022-02-11 14:32:17 +0100

    Add AsDiscordMessage interface, and all implementation

--- notifications/message.go
+++ notifications/message.go
@@ -5,6 +5,7 @@ import githubLib "github.com/google/go-github/v37/github"
 type Message interface {
 	AsSlackMessage() (*slackMessage, error)
 	AsGithubStatus() (*githubLib.RepoStatus, error)
+	AsDiscordMessage() (*discordMessage, error)
 	Env() string
 	RepositoryName() string
 	SHA() string
